fabffc04753a445657f41d660628a2ab5a040e9d
3196527843@qq.com <3196527843@qq.com>
2024-12-21 15:18:48 +0800

    ranklist

--- ranklist/del.c
+++ ranklist/del.c
@@ -1,13 +1,21 @@
-
+#include <stdlib.h>
+#include <stdio.h>
 #include "ranklist.h"
 #include "sqlite3.h"
 
-int main(const char* start_time){
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <start_time>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
     Node* head;
     sqlite3* grDB = open_database();
     head = read_from_database(grDB);
-    del_in_database(grDB, &head, start_time);
+    del_in_database(grDB, &head, argv[1]);
     update_database(grDB,head);
     sqlite3_close(grDB);
     free_all_nodes(&head);
+
+    return EXIT_SUCCESS;
 }
